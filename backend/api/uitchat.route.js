aed781fd08ed2316d2c43158f631e75b7ffb2178
vamirio256 <nguyenquocquan.nbk.9a5@gmail.com>
2022-04-03 20:24:30 +0700

    Quan - Add channel.controller.js, Add channelDAO.js

--- backend/api/uitchat.route.js
+++ backend/api/uitchat.route.js
@@ -1,16 +1,27 @@
 import express from "express";
 import UsersController from './user.controller.js';
 import server from "./server.route.js";
+import ChannelsController from "./channel.controller.js";
 
 const router = express.Router();
 
 router.use("/server", server);
 
 router
-  .route("/users")
-  .get(UsersController.apiGetUsers)
-  .post(UsersController.apiPostUser)
-  .patch(UsersController.apiPatchUser)
-  .delete(UsersController.apiDeleteUser);
+    .route("/users")
+    .get(UsersController.apiGetUsers)
+    .post(UsersController.apiPostUser)
+    .patch(UsersController.apiPatchUser)
+    .delete(UsersController.apiDeleteUser);
+
+router
+    .route('/server/:id')
+    .get(ChannelsController.apiGetChannelsByServerId);
+
+router
+    .route('/channels')
+    .put(ChannelsController.apiUpdateChannel)
+    .delete(ChannelsController.apiDeleteChannel)
+    .post(ChannelsController.apiPostChannel);
 
 export default router;
--- backend/index.js
+++ backend/index.js
@@ -3,26 +3,28 @@ import mongodb from "mongodb";
 import dotenv from "dotenv";
 import UsersDAO from './dao/usersDAO.js';
 import ServersDAO from "./dao/serversDAO.js";
+import ChannelsDAO from './dao/channelDAO.js';
 
 async function main() {
-  dotenv.config();
+    dotenv.config();
 
-  const client = new mongodb.MongoClient(process.env.UITCHAT_DB_URI);
+    const client = new mongodb.MongoClient(process.env.UITCHAT_DB_URI);
 
-  const port = process.env.PORT || 8000;
+    const port = process.env.PORT || 8000;
 
-  try {
-    await client.connect();
-    await UsersDAO.injectDB(client);
-    await ServersDAO.injectDB(client);
+    try {
+        await client.connect();
+        await UsersDAO.injectDB(client);
+        await ServersDAO.injectDB(client);
+        await ChannelsDAO.injectDB(client);
 
-    app.listen(port, () => {
-      console.log("Server is running on port", port);
-    });
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
+        app.listen(port, () => {
+            console.log("Server is running on port", port);
+        });
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
 }
 
 main().catch(console.error);
