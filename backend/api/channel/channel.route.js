831d7ad0bde124c3d5c87fb300bfc19efd600d4a
vamirio256 <nguyenquocquan.nbk.9a5@gmail.com>
2022-04-10 20:51:01 +0700

    Quan - update UserList, MessageList api

--- backend/api/channel/channel.route.js
+++ backend/api/channel/channel.route.js
@@ -3,12 +3,20 @@ import ChannelsController from "./channel.controller.js";
 
 const router = express.Router();
 
-router.route("/:id").get(ChannelsController.apiGetChannelsByServerId);
+// router.route("/:id").get(ChannelsController.apiGetChannelsByServerId);
 
 router
-  .route("/")
-  .put(ChannelsController.apiUpdateChannel)
-  .delete(ChannelsController.apiDeleteChannel)
-  .post(ChannelsController.apiPostChannel);
+    .route("/")
+    .put(ChannelsController.apiUpdateChannel)
+    .delete(ChannelsController.apiDeleteChannel)
+    .post(ChannelsController.apiPostChannel);
+
+router
+    .route("/users")
+    .get(ChannelsController.apiGetUsersByChannelId)
+    .delete(ChannelsController.apiDeleteUsersByChannelId)
+    .put(ChannelsController.apiAddUsersByChannelId);
+
+router.route("/messages").get(ChannelsController.apiGetMessagesByChannelId);
 
 export default router;
