809202057b298fa1cd1ffcfeda6c121f715d67e9
vamirio256 <nguyenquocquan.nbk.9a5@gmail.com>
2022-05-15 17:00:47 +0700

    Quan - update auth channel

--- backend/controllers/ChannelController.js
+++ backend/controllers/ChannelController.js
@@ -1,166 +1,187 @@
 import Channel from "../models/Channel.js";
 
 export default class ChannelsController {
-  static async apiGetChannelById(req, res, next) {
-    try {
-      const userId = req.userId;
-      const channelId = req.params.channelId;
-      console.log(channelId)
-      const channelResponse = await Channel.findOne({
-        _id: channelId,
-        $or: [{ memberIds: userId }, { ownerIds: userId }],
-      });
-      console.log(channelResponse)
-      return res.status(200).json({ channel: channelResponse });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+    static async apiGetChannelById(req, res, next) {
+        try {
+            const userId = req.userId;
+            const channelId = req.params.channelId;
+            console.log(channelId)
+            const channelResponse = await Channel.findOne({
+                _id: channelId,
+                $or: [{ memberIds: userId }, { ownerIds: userId }],
+            });
+            console.log(channelResponse)
+            return res.status(200).json({ channel: channelResponse });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     }
-  }
 
-  static async apiPostChannel(req, res, next) {
-    try {
-      // serverId, name, type, ownerIds, memberIds;
-      const userId = req.userId;
-      const serverId = req.body.serverId;
-      const name = req.body.name;
-      const type = req.body.type;
-      const isPublic = req.body.isPublic;
-      const ownerIds = req.body.ownerIds;
-      const memberIds = req.body.memberIds;
+    static async apiPostChannel(req, res, next) {
+        try {
+            // serverId, name, type, ownerIds, memberIds;
+            const userId = req.userId;
+            const serverId = req.body.serverId;
+            const name = req.body.name;
+            const type = req.body.type;
+            const isPublic = req.body.isPublic;
+            const ownerIds = req.body.ownerIds;
+            const memberIds = req.body.memberIds;
 
-      const channelResponse = await Channel.addChannel(
-        userId,
-        serverId,
-        name,
-        type,
-        isPublic,
-        ownerIds,
-        memberIds
-      );
-      let { error } = channelResponse;
-      if (error) {
-        return res.status(401).json({ error });
-      }
-      res.status(201).json(channelResponse);
-    } catch (e) {
-      res.status(500).json({ error: e.message });
+            const channelResponse = await Channel.addChannel(
+                userId,
+                serverId,
+                name,
+                type,
+                isPublic,
+                ownerIds,
+                memberIds
+            );
+            let { error } = channelResponse;
+            if (error) {
+                return res.status(401).json({ error });
+            }
+            res.status(201).json(channelResponse);
+        } catch (e) {
+            res.status(500).json({ error: e.message });
+        }
     }
-  }
 
-  static async apiUpdateChannel(req, res, next) {
-    try {
-      const channelId = req.body.channelId;
-      const channelName = req.body.channelName;
-      const memberId = req.member_id;
+    static async apiUpdateChannel(req, res, next) {
+        try {
+            const userId = req.body.channelId;
+            const serverId = req.body.serverId;
+            const channelId = req.body.channelId;
+            const channelName = req.body.channelName;
 
-      const channelResponse = await Channel.updateChannel(
-        channelId,
-        channelName
-      );
+            const channelResponse = await Channel.updateChannel(
+                userId,
+                serverId,
+                channelId,
+                channelName
+            );
 
-      var { error } = channelResponse;
+            var { error } = channelResponse;
 
-      if (error) {
-        res.status(400).json({ error });
-      }
+            if (error) {
+                res.status(400).json({ error });
+            }
 
-      if (channelResponse.modifiedCount === 0) {
-        throw new Error("unable to update channel");
-      }
+            if (channelResponse.modifiedCount === 0) {
+                throw new Error("unable to update channel");
+            }
 
-      res.status(204).json(channelResponse);
-    } catch (e) {
-      res.status(500).json({ error: e.message });
+            res.status(204).json(channelResponse);
+        } catch (e) {
+            res.status(500).json({ error: e.message });
+        }
     }
-  }
 
-  static async apiDeleteChannel(req, res, next) {
-    try {
-      const channelId = req.body.channelId;
+    static async apiDeleteChannel(req, res, next) {
+        try {
+            const userId = req.body.userId;
+            const serverId = req.body.serverId;
+            const channelId = req.body.channelId;
 
-      const channelResponse = await Channel.deleteChannel(channelId);
+            const channelResponse = await Channel.deleteChannel(userId, serverId, channelId);
 
-      res.status(204).json(channelResponse);
-    } catch (e) {
-      res.status(500).json({ error: e.message });
+            res.status(204).json(channelResponse);
+        } catch (e) {
+            res.status(500).json({ error: e.message });
+        }
     }
-  }
 
-  static async apiGetChannelsByServerId(req, res, next) {
-    try {
-      const serverId = req.params.serverId || {};
-      let channels = await Channel.getChannelsByServerId(serverId);
-      if (!channels) {
-        res.status(404).json({ error: "not found channels" });
-        return;
-      }
+    static async apiGetChannelsByServerId(req, res, next) {
+        try {
+            const serverId = req.params.serverId || {};
+            let channels = await Channel.getChannelsByServerId(serverId);
+            if (!channels) {
+                res.status(404).json({ error: "not found channels" });
+                return;
+            }
 
-      res.status(200).json(channels);
-    } catch (e) {
-      res.status(500).json({ error: e.message });
+            res.status(200).json(channels);
+        } catch (e) {
+            res.status(500).json({ error: e.message });
+        }
     }
-  }
 
-  static async apiUpdateMembersByChannelId(req, res, next) {
-    try {
-      const membersList = req.body.membersList;
-      const channelId = req.body.channelId;
+    static async apiUpdateMembersByChannelId(req, res, next) {
+        try {
+            const userId = req.body.userId;
+            const serverId = req.body.serverId;
+            const memberIds = req.body.memberIds;
+            const channelId = req.body.channelId;
 
-      const channelResponse = await Channel.updateMembersByChannelId(
-        channelId,
-        membersList
-      );
+            const channelResponse = await Channel.updateMembersByChannelId(
+                userId,
+                serverId,
+                channelId,
+                memberIds
+            );
 
-      res.status(204).json(channelResponse);
-    } catch (e) {
-      res.status(500).json({ error: e.message });
+            res.status(204).json(channelResponse);
+        } catch (e) {
+            res.status(500).json({ error: e.message });
+        }
     }
-  }
 
-  static async apiDeleteMembersByChannelId(req, res, next) {
-    try {
-      const membersList = req.body.membersList;
-      const channelId = req.body.channelId;
+    static async apiDeleteMembersByChannelId(req, res, next) {
+        try {
+            const userId = req.body.userId;
+            const serverId = req.body.serverId;
+            const memberIds = req.body.memberIds;
+            const channelId = req.body.channelId;
 
-      const channelResponse = await Channel.deleteMembersByChannelId(
-        channelId,
-        membersList
-      );
+            const channelResponse = await Channel.deleteMembersByChannelId(
+                userId,
+                serverId,
+                channelId,
+                memberIds
+            );
 
-      res.status(204).json(channelResponse);
-    } catch (e) {
-      res.status(500).json({ error: e.message });
+            res.status(204).json(channelResponse);
+        } catch (e) {
+            res.status(500).json({ error: e.message });
+        }
     }
-  }
 
-  static async apiUpdateOwnersByChannelId(req, res, next) {
-    try {
-      const ownersList = req.body.membersList;
-      const channelId = req.body.channelId;
+    static async apiUpdateOwnersByChannelId(req, res, next) {
+        try {
+            const userId = req.body.userId;
+            const serverId = req.body.serverId;
+            const ownerIds = req.body.ownerIds;
+            const channelId = req.body.channelId;
 
-      const channelResponse = await Channel.updateOwnersByChannelId(
-        channelId,
-        ownersList
-      );
+            const channelResponse = await Channel.updateOwnersByChannelId(
+                userId,
+                serverId,
+                channelId,
+                ownerIds
+            );
 
-      res.status(204).json(channelResponse);
-    } catch (e) {
-      res.status(500).json({ error: e.message });
+            res.status(204).json(channelResponse);
+        } catch (e) {
+            res.status(500).json({ error: e.message });
+        }
     }
-  }
 
-  static async apiDeleteOwnersByChannelId(req, res, next) {
-    try {
-      const ownersList = req.body.membersList;
-      const channelId = req.body.channelId;
+    static async apiDeleteOwnersByChannelId(req, res, next) {
+        try {
+            const userId = req.body.userId;
+            const serverId = req.body.serverId;
+            const ownerIds = req.body.ownerIds;
+            const channelId = req.body.channelId;
 
-      const channelResponse = await Channel.deleteOwnersByChannelId(
-        channelId,
-        ownersList
-      );
-      res.status(204).json(channelResponse);
-    } catch (e) {
-      res.status(500).json({ error: e.message });
+            const channelResponse = await Channel.deleteOwnersByChannelId(
+                userId,
+                serverId,
+                channelId,
+                ownerIds
+            );
+            res.status(204).json(channelResponse);
+        } catch (e) {
+            res.status(500).json({ error: e.message });
+        }
     }
-  }
 }
