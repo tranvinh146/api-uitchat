021cf95628802684e1629d9e7b2e4074f3fb6ced
Nhan <66810451+19521933@users.noreply.github.com>
2022-05-18 20:05:13 +0700

    invitation is done 🥳

--- backend/utils/Socket.js
+++ backend/utils/Socket.js
@@ -4,12 +4,34 @@ import Invitation from "../models/Invitation.js";
 export default function socket(io) {
 	io.on('connection', (socket) => {
 
+		// LOGIN/LOGOUT
+		// login
 		console.log(`User ${socket.id} has connected.`);
 
-		socket.on("disconnect", () => {
-			console.log(`User ${socket.id} has disconnected.`);
+		socket.on("login", async (userId) => {
+			try {
+				const user = await User.findById(userId);
+				user.socketId = socket.id;
+				user.status = "online";
+				await user.save();
+			} catch (error) {
+				console.log(error.message);
+			}
 		});
 
+		socket.on("disconnect", async () => {
+			console.log(`User ${socket.id} has disconnected.`);
+			try {
+				const user = await User.findOne({ socketId: socket.id });
+				user.socketId = null;
+				user.status = "offline";
+				await user.save();
+			} catch (error) {
+				console.log(error.message);
+			}
+		});
+
+		// INVITATION
 		socket.on("invite", async (invitation) => {
 			try {
 				const receiver = await User.findById(invitation.receiverId);
@@ -23,7 +45,7 @@ export default function socket(io) {
 				});
 
 				io.to(receiver.socketId).emit("have new invitation", populatedInvitation);
-				socket.emit("invitation has been sent successfully");
+				socket.emit("successfully sent invitation");
 			} catch (error) {
 				socket.emit("failed to send invitation", error.message);
 			}
@@ -43,8 +65,9 @@ export default function socket(io) {
 				await Invitation.removeInvitation(invitation.receiverId._id, invitationId);
 				await User.joinServer(invitation.receiverId._id, invitation.serverId._id);
 
-				socket.emit("invitation responded");
+				socket.emit("successfully accepted invitation");
 				io.to(invitation.senderId.socketId).emit("invitation responded");
+				// emit to server -> re-render member list
 
 			} catch (error) {
 				socket.emit("failed to accept invitation", error.message);
@@ -63,7 +86,7 @@ export default function socket(io) {
 
 				await Invitation.removeInvitation(invitation.receiverId._id, invitationId);
 
-				socket.emit("invitation responded");
+				socket.emit("successfully declined invitation");
 				io.to(invitation.senderId.socketId).emit("invitation responded");
 
 			} catch (error) {
