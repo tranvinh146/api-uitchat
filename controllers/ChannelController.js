1079dce4ae5948f1cf69afc7e7f291afa8bc012c
tran vinh <tranvinhlx135@gmail.com>
2022-05-23 22:17:48 +0700

    test

--- controllers/ChannelController.js
+++ controllers/ChannelController.js
@@ -1,175 +1,171 @@
 import Channel from "../models/Channel.js";
 
 export default class ChannelsController {
-    static async apiGetChannelById(req, res, next) {
-        try {
-            const channelId = req.params.channelId;
-            const channel = await Channel.getChannelById(channelId);
-
-            return res.status(200).json(channel);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
+  static async apiGetChannelById(req, res, next) {
+    try {
+      const channelId = req.params.channelId;
+      const channel = await Channel.getChannelById(channelId);
+      return res.status(200).json(channel);
+    } catch (error) {
+      res.status(500).json({ errors: error.message });
     }
+  }
 
-    static async apiPostChannel(req, res, next) {
-        try {
-            // serverId, name, type, ownerIds, memberIds;
-            const userId = req.userId;
-            const serverId = req.body.serverId;
-            const name = req.body.name;
-            const type = req.body.type;
-            const isPublic = req.body.isPublic;
-            const ownerIds = req.body.ownerIds;
-            const memberIds = req.body.memberIds;
+  static async apiPostChannel(req, res, next) {
+    try {
+      // serverId, name, type, ownerIds, memberIds;
+      const userId = req.userId;
+      const serverId = req.body.serverId;
+      const name = req.body.name;
+      const type = req.body.type;
+      const isPublic = req.body.isPublic;
+      const ownerIds = req.body.ownerIds;
+      const memberIds = req.body.memberIds;
 
-            const channelResponse = await Channel.addChannel(
-                userId,
-                serverId,
-                name,
-                type,
-                isPublic,
-                ownerIds,
-                memberIds
-            );
-            let { error } = channelResponse;
-            if (error) {
-                return res.status(401).json({ error });
-            }
-            res.status(201).json(channelResponse);
-        } catch (e) {
-            res.status(500).json({ error: e.message });
-        }
+      const channelResponse = await Channel.addChannel(
+        userId,
+        serverId,
+        name,
+        type,
+        isPublic,
+        ownerIds,
+        memberIds
+      );
+      let { error } = channelResponse;
+      if (error) {
+        return res.status(401).json({ error });
+      }
+      res.status(201).json(channelResponse);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
     }
+  }
 
-    static async apiUpdateChannel(req, res, next) {
-        try {
-            const userId = req.channelId;
-            const channelId = req.body.channelId;
-            const channelName = req.body.channelName;
+  static async apiUpdateChannel(req, res, next) {
+    try {
+      const userId = req.channelId;
+      const channelId = req.body.channelId;
+      const channelName = req.body.channelName;
 
-            const channelResponse = await Channel.updateChannel(
-                userId,
-                channelId,
-                channelName
-            );
+      const channelResponse = await Channel.updateChannel(
+        userId,
+        channelId,
+        channelName
+      );
 
-            var { error } = channelResponse;
+      var { error } = channelResponse;
 
-            if (error) {
-                res.status(400).json({ error });
-            }
+      if (error) {
+        res.status(400).json({ error });
+      }
 
-            if (channelResponse.modifiedCount === 0) {
-                throw new Error("unable to update channel");
-            }
+      if (channelResponse.modifiedCount === 0) {
+        throw new Error("unable to update channel");
+      }
 
-            res.status(204).json(channelResponse);
-        } catch (e) {
-            res.status(500).json({ error: e.message });
-        }
+      res.status(204).json(channelResponse);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
     }
+  }
 
-    static async apiDeleteChannel(req, res, next) {
-        try {
-            const userId = req.userId;
-            const channelId = req.body.channelId;
+  static async apiDeleteChannel(req, res, next) {
+    try {
+      const userId = req.userId;
+      const channelId = req.body.channelId;
 
-            const channelResponse = await Channel.deleteChannel(
-                userId,
-                channelId
-            );
+      const channelResponse = await Channel.deleteChannel(userId, channelId);
 
-            res.status(204).json(channelResponse);
-        } catch (e) {
-            res.status(500).json({ error: e.message });
-        }
+      res.status(204).json(channelResponse);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
     }
+  }
 
-    static async apiGetChannelsByServerId(req, res, next) {
-        try {
-            const serverId = req.params.serverId || {};
-            let channels = await Channel.getChannelsByServerId(serverId);
-            if (!channels) {
-                res.status(404).json({ error: "not found channels" });
-                return;
-            }
+  static async apiGetChannelsByServerId(req, res, next) {
+    try {
+      const serverId = req.params.serverId || {};
+      let channels = await Channel.getChannelsByServerId(serverId);
+      if (!channels) {
+        res.status(400).json({ error: "not found channels" });
+        return;
+      }
 
-            res.status(200).json(channels);
-        } catch (e) {
-            res.status(500).json({ error: e.message });
-        }
+      res.status(200).json(channels);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
     }
+  }
 
-    static async apiUpdateMembersByChannelId(req, res, next) {
-        try {
-            const userId = req.userId;
-            const memberIds = req.body.memberIds;
-            const channelId = req.body.channelId;
+  static async apiUpdateMembersByChannelId(req, res, next) {
+    try {
+      const userId = req.userId;
+      const memberIds = req.body.memberIds;
+      const channelId = req.body.channelId;
 
-            const channelResponse = await Channel.updateMembersByChannelId(
-                userId,
-                channelId,
-                memberIds
-            );
+      const channelResponse = await Channel.updateMembersByChannelId(
+        userId,
+        channelId,
+        memberIds
+      );
 
-            res.status(204).json(channelResponse);
-        } catch (e) {
-            res.status(500).json({ error: e.message });
-        }
+      res.status(204).json(channelResponse);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
     }
+  }
 
-    static async apiDeleteMembersByChannelId(req, res, next) {
-        try {
-            const userId = req.userId;
-            const memberIds = req.body.memberIds;
-            const channelId = req.body.channelId;
+  static async apiDeleteMembersByChannelId(req, res, next) {
+    try {
+      const userId = req.userId;
+      const memberIds = req.body.memberIds;
+      const channelId = req.body.channelId;
 
-            const channelResponse = await Channel.deleteMembersByChannelId(
-                userId,
-                channelId,
-                memberIds
-            );
+      const channelResponse = await Channel.deleteMembersByChannelId(
+        userId,
+        channelId,
+        memberIds
+      );
 
-            res.status(204).json(channelResponse);
-        } catch (e) {
-            res.status(500).json({ error: e.message });
-        }
+      res.status(204).json(channelResponse);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
     }
+  }
 
-    static async apiUpdateOwnersByChannelId(req, res, next) {
-        try {
-            const userId = req.userId;
-            const ownerIds = req.body.ownerIds;
-            const channelId = req.body.channelId;
+  static async apiUpdateOwnersByChannelId(req, res, next) {
+    try {
+      const userId = req.userId;
+      const ownerIds = req.body.ownerIds;
+      const channelId = req.body.channelId;
 
-            const channelResponse = await Channel.updateOwnersByChannelId(
-                userId,
-                channelId,
-                ownerIds
-            );
+      const channelResponse = await Channel.updateOwnersByChannelId(
+        userId,
+        channelId,
+        ownerIds
+      );
 
-            res.status(204).json(channelResponse);
-        } catch (e) {
-            res.status(500).json({ error: e.message });
-        }
+      res.status(204).json(channelResponse);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
     }
+  }
 
-    static async apiDeleteOwnersByChannelId(req, res, next) {
-        try {
-            const userId = req.userId;
-            const ownerIds = req.body.ownerIds;
-            const channelId = req.body.channelId;
+  static async apiDeleteOwnersByChannelId(req, res, next) {
+    try {
+      const userId = req.userId;
+      const ownerIds = req.body.ownerIds;
+      const channelId = req.body.channelId;
 
-            const channelResponse = await Channel.deleteOwnersByChannelId(
-                userId,
+      const channelResponse = await Channel.deleteOwnersByChannelId(
+        userId,
 
-                channelId,
-                ownerIds
-            );
-            res.status(204).json(channelResponse);
-        } catch (e) {
-            res.status(500).json({ error: e.message });
-        }
+        channelId,
+        ownerIds
+      );
+      res.status(204).json(channelResponse);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
     }
+  }
 }
--- models/Channel.js
+++ models/Channel.js
@@ -3,305 +3,302 @@ import Server from "./Server.js";
 import User from "./User.js";
 
 const channelSchema = new mongoose.Schema(
-    {
-        serverId: {
-            type: mongoose.SchemaTypes.ObjectId,
-            required: true,
-            ref: "Server"
-        },
-        name: {
-            type: String,
-            required: true,
-        },
-        isPublic: {
-            type: Boolean,
-            required: true,
-        },
-        type: {
-            type: String,
-            // enum: ["text", "voice"],
-        },
-        ownerIds: { type: [mongoose.Types.ObjectId], ref: "User" },
-        memberIds: { type: [mongoose.Types.ObjectId], ref: "User" },
+  {
+    serverId: {
+      type: mongoose.SchemaTypes.ObjectId,
+      required: true,
+      ref: "Server",
     },
-    {
-        timestamps: true,
-    }
+    name: {
+      type: String,
+      required: true,
+    },
+    isPublic: {
+      type: Boolean,
+      required: true,
+    },
+    type: {
+      type: String,
+      // enum: ["text", "voice"],
+    },
+    ownerIds: { type: [mongoose.Types.ObjectId], ref: "User" },
+    memberIds: { type: [mongoose.Types.ObjectId], ref: "User" },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 channelSchema.statics.getChannelById = async function (channelId) {
-    try {
-        return await this.findById(channelId)
-            .populate({
-                path: "memberIds",
-                select: ["_id", "email", "name", "avatar"],
-            })
-            .populate({
-                path: "ownerIds",
-                select: ["_id", "email", "name", "avatar"],
-            });
-    } catch (error) {
-        console.error(`something went wrong in getChannelById: ${error}`);
-        throw error;
-    }
+  try {
+    return await this.findById(channelId)
+      .populate({
+        path: "memberIds",
+        select: ["_id", "email", "name", "avatar"],
+      })
+      .populate({
+        path: "ownerIds",
+        select: ["_id", "email", "name", "avatar"],
+      });
+  } catch (error) {
+    console.error(`something went wrong in getChannelById: ${error}`);
+    throw error;
+  }
 };
 
 channelSchema.statics.addChannel = async function (
-    userId, // backend added
-    serverId,
-    name,
-    type,
-    isPublic,
-    ownerIds,
-    memberIds
+  userId, // backend added
+  serverId,
+  name,
+  type,
+  isPublic,
+  ownerIds,
+  memberIds
 ) {
-    try {
-        //checking if userId is in ownerIds of server
-        let server = await Server.findById(serverId).populate('ownerIds', '_id');
-        if (!server.ownerIds._id.includes(userId)) {
-            return { error: "You may not have permisson." };
-        }
-
-        let channel;
-
-        if (isPublic) {
-            channel = await this.create({
-                serverId,
-                name,
-                type,
-                isPublic,
-                ownerIds: currentServer.ownerIds,
-                memberIds: currentServer.memberIds,
-            });
-        } else {
-            channel = await this.create({
-                serverId,
-                name,
-                type,
-                isPublic,
-                ownerIds,
-                memberIds,
-            });
-        }
-
-        //get user socketId
-        let socket = await User.findById(userId).socketId;
-
-        //emit to frontend
-        socket.emit("add-channel-success");
-        socket.to(channel.serverId).emit("add-channel-success");
-
-        return channel;
-    } catch (e) {
-        console.error(`somthing went wrong in addChannel: ${e.message}`);
-        return { error: e.message };
+  try {
+    //checking if userId is in ownerIds of server
+    let server = await Server.findById(serverId).populate("ownerIds", "_id");
+    if (!server.ownerIds._id.includes(userId)) {
+      return { error: "You may not have permisson." };
     }
+
+    let channel;
+
+    if (isPublic) {
+      channel = await this.create({
+        serverId,
+        name,
+        type,
+        isPublic,
+        ownerIds: currentServer.ownerIds,
+        memberIds: currentServer.memberIds,
+      });
+    } else {
+      channel = await this.create({
+        serverId,
+        name,
+        type,
+        isPublic,
+        ownerIds,
+        memberIds,
+      });
+    }
+
+    //get user socketId
+    let socket = await User.findById(userId).socketId;
+
+    //emit to frontend
+    socket.emit("add-channel-success");
+    socket.to(channel.serverId).emit("add-channel-success");
+
+    return channel;
+  } catch (e) {
+    console.error(`somthing went wrong in addChannel: ${e.message}`);
+    return { error: e.message };
+  }
 };
 
-channelSchema.statics.deleteChannel = async function (
-    userId,
-    channelId
-) {
-    try {
-        //checking if userId in ownerIds of channel 
-        let channel = await this.findById(channelId).populate("ownerIds", "_id");
-        if (!channel.ownerIds._id.includes(userId)) {
-            return { error: "You may not have permisson." };
-        }
-
-        const deleteResponse = await this.deleteOne({
-            _id: ObjectId(channelId),
-            owerIds: { $in: memberId },
-        });
-
-        //get user socketId
-        let socket = await User.findById(userId).socketId;
-
-        //emit to frontend
-        socket.emit("delete-channel-success");
-        socket.to(channel.serverId).emit("delete-channel-success");
-
-        return deleteResponse;
-    } catch (e) {
-        console.error(`somthing went wrong in deleteChannel: ${e.message}`);
-        return { error: e.message };
+channelSchema.statics.deleteChannel = async function (userId, channelId) {
+  try {
+    //checking if userId in ownerIds of channel
+    let channel = await this.findById(channelId).populate("ownerIds", "_id");
+    if (!channel.ownerIds._id.includes(userId)) {
+      return { error: "You may not have permisson." };
     }
+
+    const deleteResponse = await this.deleteOne({
+      _id: ObjectId(channelId),
+      owerIds: { $in: memberId },
+    });
+
+    //get user socketId
+    let socket = await User.findById(userId).socketId;
+
+    //emit to frontend
+    socket.emit("delete-channel-success");
+    socket.to(channel.serverId).emit("delete-channel-success");
+
+    return deleteResponse;
+  } catch (e) {
+    console.error(`somthing went wrong in deleteChannel: ${e.message}`);
+    return { error: e.message };
+  }
 };
 
 channelSchema.statics.updateChannel = async function (
-    userId,
-    channelId,
-    channelName
+  userId,
+  channelId,
+  channelName
 ) {
-    try {
-        //checking if userId in ownerIds of channel 
-        let channel = await this.findById(channelId).populate("ownerIds", "_id");
-        if (!channel.ownerIds._id.includes(userId)) {
-            return { error: "You may not have permisson." };
-        }
-
-        const updateResponse = await this.updateOne(
-            { _id: ObjectId(channelId) },
-            { $set: { name: channelName } }
-        );
-
-        //get user socketId
-        let socket = await User.findById(userId).socketId;
-
-        //emit to frontend
-        socket.emit("update-channel-success");
-        socket.to(channel.serverId).emit("update-channel-success");
-
-        return updateResponse;
-    } catch (e) {
-        console.error(`somthing went wrong in updateChannel: ${e.message}`);
-        return { error: e.message };
+  try {
+    //checking if userId in ownerIds of channel
+    let channel = await this.findById(channelId).populate("ownerIds", "_id");
+    if (!channel.ownerIds._id.includes(userId)) {
+      return { error: "You may not have permisson." };
     }
+
+    const updateResponse = await this.updateOne(
+      { _id: ObjectId(channelId) },
+      { $set: { name: channelName } }
+    );
+
+    //get user socketId
+    let socket = await User.findById(userId).socketId;
+
+    //emit to frontend
+    socket.emit("update-channel-success");
+    socket.to(channel.serverId).emit("update-channel-success");
+
+    return updateResponse;
+  } catch (e) {
+    console.error(`somthing went wrong in updateChannel: ${e.message}`);
+    return { error: e.message };
+  }
 };
 
 channelSchema.statics.getChannelsByServerId = async function (serverId) {
-    try {
-        let channels = await this.findById(serverId)
-            .populate({
-                path: "memberIds",
-                select: ["_id", "email", "name", "avatar"],
-            })
-            .populate({
-                path: "ownerIds",
-                select: ["_id", "email", "name", "avatar"],
-            });
+  try {
+    let channels = await this.find({ serverId })
+      .populate({
+        path: "memberIds",
+        select: ["_id", "email", "name", "avatar"],
+      })
+      .populate({
+        path: "ownerIds",
+        select: ["_id", "email", "name", "avatar"],
+      });
 
-        return channels;
-    } catch (e) {
-        console.error(`something went wrong in getChannelByServerId: ${e.message}`);
-        throw e.message;
-    }
+    return channels;
+  } catch (e) {
+    console.error(`something went wrong in getChannelByServerId: ${e.message}`);
+    throw e.message;
+  }
 };
 
 channelSchema.statics.deleteMembersByChannelId = async function (
-    userId,
-    channelId,
-    memberIds
+  userId,
+  channelId,
+  memberIds
 ) {
-    try {
-        //checking if userId in ownerIds of channel 
-        let channel = await this.findById(channelId).populate("ownerIds", "_id");
-        if (!channel.ownerIds._id.includes(userId)) {
-            return { error: "You may not have permisson." };
-        }
-
-        const deleteResponse = await this.updateOne(
-            { _id: ObjectId(channelId) },
-            { $pull: { memberIds: { $in: memberIds } } }
-        );
-
-        //get user socketId
-        let socket = await User.findById(userId).socketId;
-
-        //emit to frontend
-        socket.emit("delete-member-in-channel-success");
-        socket.to(channel.serverId).emit("delete-member-in-channel-success");
-
-        return deleteResponse;
-    } catch (e) {
-        console.error(
-            `somthing went wrong in deleteMembersByChannelId: ${e.message}`
-        );
-        return { error: e.message };
+  try {
+    //checking if userId in ownerIds of channel
+    let channel = await this.findById(channelId).populate("ownerIds", "_id");
+    if (!channel.ownerIds._id.includes(userId)) {
+      return { error: "You may not have permisson." };
     }
+
+    const deleteResponse = await this.updateOne(
+      { _id: ObjectId(channelId) },
+      { $pull: { memberIds: { $in: memberIds } } }
+    );
+
+    //get user socketId
+    let socket = await User.findById(userId).socketId;
+
+    //emit to frontend
+    socket.emit("delete-member-in-channel-success");
+    socket.to(channel.serverId).emit("delete-member-in-channel-success");
+
+    return deleteResponse;
+  } catch (e) {
+    console.error(
+      `somthing went wrong in deleteMembersByChannelId: ${e.message}`
+    );
+    return { error: e.message };
+  }
 };
 
 channelSchema.statics.updateMembersByChannelId = async function (
-    userId,
-    channelId,
-    memberIds
+  userId,
+  channelId,
+  memberIds
 ) {
-    try {
-        //checking if userId in ownerIds of channel 
-        let channel = await this.findById(channelId).populate("ownerIds", "_id");
-        if (!channel.ownerIds._id.includes(userId)) {
-            return { error: "You may not have permisson." };
-        }
-
-        const updateResponse = await this.updateOne(
-            { _id: ObjectId(channelId) },
-            { $addToSet: { memberIds: { $each: memberIds } } }
-        );
-
-        //get user socketId
-        let socket = await User.findById(userId).socketId;
-
-        //emit to frontend
-        socket.emit("update-member-in-channel-success");
-        socket.to(channel.serverId).emit("update-member-in-channel-success");
-
-        return updateResponse;
-    } catch (e) {
-        console.error(`something went wront in updateMembersByChannelId:${e}`);
-        throw e;
+  try {
+    //checking if userId in ownerIds of channel
+    let channel = await this.findById(channelId).populate("ownerIds", "_id");
+    if (!channel.ownerIds._id.includes(userId)) {
+      return { error: "You may not have permisson." };
     }
+
+    const updateResponse = await this.updateOne(
+      { _id: ObjectId(channelId) },
+      { $addToSet: { memberIds: { $each: memberIds } } }
+    );
+
+    //get user socketId
+    let socket = await User.findById(userId).socketId;
+
+    //emit to frontend
+    socket.emit("update-member-in-channel-success");
+    socket.to(channel.serverId).emit("update-member-in-channel-success");
+
+    return updateResponse;
+  } catch (e) {
+    console.error(`something went wront in updateMembersByChannelId:${e}`);
+    throw e;
+  }
 };
 
 channelSchema.statics.deleteOwnersByChannelId = async function (
-    userId,
-    channelId,
-    ownerIds
+  userId,
+  channelId,
+  ownerIds
 ) {
-    try {
-        //checking if userId in ownerIds of channel 
-        let channel = await this.findById(channelId).populate("ownerIds", "_id");
-        if (!channel.ownerIds._id.includes(userId)) {
-            return { error: "You may not have permisson." };
-        }
-
-        const deleteResponse = await this.updateOne(
-            { _id: ObjectId(channelId) },
-            { $pull: { ownerIds: { $in: ownerIds } } }
-        );
-
-        //get user socketId
-        let socket = await User.findById(userId).socketId;
-
-        //emit to frontend
-        socket.emit("delete-owner-in-channel-success");
-        socket.to(channel.serverId).emit("delete-owner-in-channel-success");
-
-        return deleteResponse;
-    } catch (e) {
-        console.error(`unable to delete members: ${e}`);
-        return { error: e };
+  try {
+    //checking if userId in ownerIds of channel
+    let channel = await this.findById(channelId).populate("ownerIds", "_id");
+    if (!channel.ownerIds._id.includes(userId)) {
+      return { error: "You may not have permisson." };
     }
+
+    const deleteResponse = await this.updateOne(
+      { _id: ObjectId(channelId) },
+      { $pull: { ownerIds: { $in: ownerIds } } }
+    );
+
+    //get user socketId
+    let socket = await User.findById(userId).socketId;
+
+    //emit to frontend
+    socket.emit("delete-owner-in-channel-success");
+    socket.to(channel.serverId).emit("delete-owner-in-channel-success");
+
+    return deleteResponse;
+  } catch (e) {
+    console.error(`unable to delete members: ${e}`);
+    return { error: e };
+  }
 };
 
 channelSchema.statics.updateOwnersByChannelId = async function (
-    userId,
-    channelId,
-    ownerIds
+  userId,
+  channelId,
+  ownerIds
 ) {
-    try {
-        //checking if userId in ownerIds of channel 
-        let channel = await this.findById(channelId).populate("ownerIds", "_id");
-        if (!channel.ownerIds._id.includes(userId)) {
-            return { error: "You may not have permisson." };
-        }
-
-        const updateResponse = await this.updateOne(
-            { _id: ObjectId(channelId) },
-            { $addToSet: { ownerIds: { $each: ownerIds } } }
-        );
-
-        //get user socketId
-        let socket = await User.findById(userId).socketId;
-
-        //emit to frontend
-        socket.emit("update-owner-in-channel-success");
-        socket.to(channel.serverId).emit("update-owner-in-channel-success");
-
-        return updateResponse;
-    } catch (e) {
-        console.error(`something went wront in updateMembersByChannelId:${e}`);
-        throw e;
+  try {
+    //checking if userId in ownerIds of channel
+    let channel = await this.findById(channelId).populate("ownerIds", "_id");
+    if (!channel.ownerIds._id.includes(userId)) {
+      return { error: "You may not have permisson." };
     }
+
+    const updateResponse = await this.updateOne(
+      { _id: ObjectId(channelId) },
+      { $addToSet: { ownerIds: { $each: ownerIds } } }
+    );
+
+    //get user socketId
+    let socket = await User.findById(userId).socketId;
+
+    //emit to frontend
+    socket.emit("update-owner-in-channel-success");
+    socket.to(channel.serverId).emit("update-owner-in-channel-success");
+
+    return updateResponse;
+  } catch (e) {
+    console.error(`something went wront in updateMembersByChannelId:${e}`);
+    throw e;
+  }
 };
 
 const Channel = mongoose.model("Channel", channelSchema);
