1e5ddbc41bf9c66c89009754a8e7b72aa52374cf
tran vinh <tranvinhlx135@gmail.com>
2022-05-25 22:11:22 +0700

    .env

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ import app from "./server.js";
 import connectdb from "./config/mongo.js";
 import dotenv from "dotenv";
 import { Server } from "socket.io";
-import http from 'http';
+import http from "http";
 import socket from "./utils/Socket.js";
 
 async function main() {
-	dotenv.config();
-	connectdb();
+  dotenv.config();
+  connectdb();
 
-	const server = http.createServer(app);
-	const io = new Server(server, {
-		cors: {
-			origin: "*"
-		}
-	});
+  const server = http.createServer(app);
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+    },
+  });
 
-	socket(io);
+  socket(io);
 
-	const port = process.env.PORT || 8000;
+  const port = process.env.PORT || 8000;
 
-	server.listen(port, () => {
-		console.log("Server is running on port", port);
-	});
+  server.listen(port, () => {
+    console.log("Server is running on port", port);
+  });
 }
 
 main().catch(console.error);
