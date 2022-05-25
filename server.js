417d610bfd2abebcb5e3afef9bc98bf5f0a0c1b3
tran vinh <tranvinhlx135@gmail.com>
2022-05-25 22:28:22 +0700

    asdfasfd

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
