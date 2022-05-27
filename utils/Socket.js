91bdae72df7ce0d2ae6b697375ff96bfbf149823
Le Duc Thang <19522197@gm.uit.edu.vn>
2022-05-27 17:35:48 +0700

    thang add socket deleteServer

--- utils/Socket.js
+++ utils/Socket.js
@@ -33,6 +33,12 @@ export default function socket(io) {
       });
       io.to(serverId).emit("updated-server", server);
     });
+
+    socket.on("delete-server" , async({serverId}) => {
+      await Server.deleteServer(serverId, userId)
+      io.to(serverId).emit("deleted-server", serverId)
+    })
+
     // =================================================
 
     // ================= MESSAGE =======================
